7ef348cc70aa1d7ecd62174d3e84e3d9f556bc2c
Jeet Khondker <jeet.usolutionsgroup@gmail.com>
2020-08-29 07:35:13 +0900

    入力Validationチェックの追加

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -1,8 +1,27 @@
 <template>
     <div id="employee-form">
         <form @submit.prevent="handleSubmit">
-            <input v-model="employee.name" type="text" placeholder="新社員名" />
-            <input v-model="employee.email" type="text" placeholder="新社員メール" />
+            <input 
+                v-model="employee.name" 
+                type="text" 
+                placeholder="新社員名" 
+                :class="{ 'has-error': submitting && invalidName }" 
+                @focus="clearStatus" 
+                @keypress="clearStatus"
+            />
+            <input 
+                v-model="employee.email" 
+                type="text" 
+                placeholder="新社員メール" 
+                :class="{ 'has-error': submitting && invalidEmail }" 
+                @focus="clearStatus" 
+            />
+            <p v-if="error && submitting" class="error-message">
+                ❌ 全項目を入力してください！
+            </p>
+            <p v-if="success" class="success-message">
+                ✅ 新社員が成功に登録されました！
+            </p>
             <button>新社員登録</button>
         </form>
     </div>
@@ -13,6 +32,9 @@
         name: "employee-form",
         data() {
             return {
+                submitting: false,
+                error: false,
+                success: false,
                 employee: {
                     name: '',
                     email: '',
@@ -21,7 +43,38 @@
         },
         methods: {
             handleSubmit() {
+                this.submitting = true
+                this.clearStatus()
+
+                if (this.invalidName || this.invalidEmail) {
+                    this.error = true
+                    return
+                }
+
                 this.$emit("addemployee", this.employee)
+
+                this.employee = {
+                    name: '',
+                    email: '',
+                }
+
+                this.error = false
+                this.success = true
+                this.submitting = false
+            },
+
+            clearStatus() {
+                this.success = false
+                this.error = false
+            }
+        },
+        computed: {
+            invalidName() {
+                return this.employee.name === ''
+            },
+
+            invalidEmail() {
+                return this.employee.email === ''
             },
         },
     }
@@ -35,8 +88,23 @@
     input {
         border: 1px solid #000;
     }
-
     input:hover {
         border: 1px solid #013cff;
+    }
+
+    [class*="-message"] {
+        font-weight: 500;
+    }
+
+    .error-message {
+        color: #000;
+        background: #f8adaf;
+        padding: 10px;
+    }
+
+    .success-message {
+        color: #000;
+        padding: 10px;
+        background: #84fdb1;
     }
 </style>
