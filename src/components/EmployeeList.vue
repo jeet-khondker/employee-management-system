f8ef7d2e03d109c29869b4999d35ebf2a0a15fca
Jeet Khondker <jeet.usolutionsgroup@gmail.com>
2020-08-28 12:05:15 +0900

    社員リストの作成

---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="employee-list">
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        社員名
+                    </th>
+                    <th>
+                        社員メール
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        桑村
+                    </td>
+                    <td>
+                        tokio.kuwamura@code.com
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        鈴木
+                    </td>
+                    <td>
+                        ayumu.suzuki@code.com
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        長友
+                    </td>
+                    <td>
+                        syouya.nagatomo@code.com
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "employee-list",
+    }
+</script>
+
+<style scoped>
+
+</style>
