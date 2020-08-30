876790b7cd40119585961d9a0fa3fb3151dfc4f4
Jeet Z. H. Khondker <48243450+jeet-khondker@users.noreply.github.com>
2020-08-30 19:17:58 +0900

    セキュリティーのため、APIキーとセンダーID

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import router from './router'
 import firebase from "firebase"
 
 const config = {
-    apiKey: "AIzaSyBvd_sNPS3lksyshV2t35YRyJpqO9adR_o",
+    apiKey: "key-that-cannot-be-shared",
     authDomain: "employee-manager-9acf8.firebaseapp.com",
     databaseURL: "https://employee-manager-9acf8.firebaseio.com",
     projectId: "employee-manager-9acf8",
     storageBucket: "employee-manager-9acf8.appspot.com",
-    messagingSenderId: "420642554993"
+    messagingSenderId: "xxxxxxxxxxxx"
 };
 
 firebase.initializeApp(config)
